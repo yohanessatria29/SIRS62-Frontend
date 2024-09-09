0df56d338489448afe026a5032042de44b61accc
nasrulfata <nasrul10fata@gmail.com>
2024-09-09 12:44:54 +0700

    update

--- src/components/RL317/RL317.js
+++ src/components/RL317/RL317.js
@@ -475,6 +475,7 @@ export const RL317 = () => {
 
     return (
         <div className="container" style={{ marginTop: "70px", marginBottom: "70px" }}>
+           <h4 style={{  color: "grey" }}> <span>  RL 3.17 Farmasi Pengadaan Obat </span></h4>
         <Modal show={show} onHide={handleClose} style={{position: "fixed"}}>
             <Modal.Header closeButton>
                 <Modal.Title>Filter</Modal.Title>
@@ -693,9 +694,7 @@ export const RL317 = () => {
                     </button>
                     <button className='btn' style={{ fontSize: "18px", marginLeft: "5px", backgroundColor: "#779D9E", color: "#FFFFFF" }} onClick={handleDownloadExcel}>Download</button>
                 </div>
-                <div className="col-md-2" style={{ fontSize: "14px", backgroundColor: "#779D9E", color: "#FFFFFF"}}>
-                RL 3.17 Farmasi Pengadaan Obat
-                        </div>
+
                 <div>
                     <h5 style={{fontSize: "14px"}}>
                         filtered by {filterLabel.map((value) => {
@@ -735,9 +734,12 @@ export const RL317 = () => {
                                             user.jenisUserId === 4 ? (
                                                 <div style={{ display: "flex" }}>
                                                     <button className="btn btn-danger" style={{ margin: "0 5px 0 0", backgroundColor: "#FF6663", border: "1px solid #FF6663" }} type='button' onClick={(e) => deleteConfirmation(value.id)}>Hapus</button>
+                                                    {value.no_golongan_obat==="0" ?
+                                                    '': 
                                                     <Link to={`/rl317/ubah/${value.id}`} className='btn btn-warning' style={{ margin: "0 5px 0 0", backgroundColor: "#CFD35E", border: "1px solid #CFD35E", color: "#FFFFFF" }} >
                                                         Ubah
                                                     </Link>
+                                                    }
                                                 </div>
                                                 
                                             ) : (
